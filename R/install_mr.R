8308a51734fdcc517e64e547ead8b885fa83780f
kexhong <675245820@qq.com>
2022-11-24 21:40:35 +0800

    first

---
+++ R/install_mr.R
@@ -0,0 +1,24 @@
+
+#' install pkgs
+#'
+#' @export install_mr
+#'
+#' @examples install_mr()
+install_mr <- function(){
+  packages <- c("usethis", "devtools", "remotes", "data.table", "do", "eoffice")
+
+  for (i in 1:length(packages)) {
+    if (!packages[i] %in% installed.packages()[,"Package"]) {
+      install.packages(packages[i], dependencies = TRUE, quiet = TRUE, keep_outputs=TRUE)
+    }
+  }
+
+  remotes::install_github("MRCIEU/TwoSampleMR")
+  devtools::install_github("kexhong/MendelR",
+                           auth_token = "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",
+                           force = TRUE, quiet=TRUE)
+
+  library(TwoSampleMR)
+  library(MendelR)
+  mr_o2o("ieu-b-2", "ieu-a-2", write_csv=FALSE, write_ppt=FALSE)
+}
