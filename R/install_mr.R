8f8b994b6c7112d15063820e86a5ab8daf9ac22e
kexhong <675245820@qq.com>
2022-11-24 23:03:20 +0800

    Update install_mr.R

--- R/install_mr.R
+++ R/install_mr.R
@@ -15,7 +15,7 @@ install_mr <- function(){
 
   remotes::install_github("MRCIEU/TwoSampleMR", upgrade=c("never"), quiet=TRUE)
 
-  auth_token = "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"
+  auth_token = "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"
 
   devtools::install_github("kexhong/MendelR",
                            auth_token = auth_token,
