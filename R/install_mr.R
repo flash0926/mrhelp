5e39b8b1ab5ea6cc4a490f36ec6f992bde263636
flash0926 <675245820@qq.com>
2022-12-16 22:40:58 +0800

    Update install_mr.R

--- R/install_mr.R
+++ R/install_mr.R
@@ -20,7 +20,7 @@ install_mr <- function(){
   e <- tryCatch(detach("package:MendelR", unload = TRUE),
                 error = function(e) "e")
 
-  auth_token = paste("github_pat", "11ADKTPDY0TN15yF8qGkeJ_vl8HLnJP4CWoqUedPd3pgsfZpVDm5WRbBc9cF5jaNfrUP3WWRGAspdlKaNi", sep='_')
+  auth_token = paste("github_pat", "11ADKTPDY0hXrx6BSiabOE_KnDHoiTfvKTPYpi1DFusRwNpcX7sxYBvvKL0tOnAzy4K47I6MCJpHjsgnDX", sep='_')
 
   remotes::install_github("flash0926/MendelR",
                            auth_token = auth_token,
