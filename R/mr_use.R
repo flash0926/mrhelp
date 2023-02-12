2bfe95248ae15891d0857138c495181f6fd489fa
flash0926 <675245820@qq.com>
2023-02-12 18:28:54 +0800

    1

--- R/mr_use.R
+++ R/mr_use.R
@@ -6,16 +6,18 @@
 #'
 mr_use <- function(){
   message("常用方法")
-  message("1.两个ieu编号方法")
-  message("mr_o2o(\"ieu-a-2\", \"ieu-a-2\")")
-  message("2.第一个参数是本地的csv文件, 第二个是ieu编号")
-  message("mr_l2o(\"snp_file\", \"ieu-a-2\")")
-  message("3.两个都是本地csv文件")
-  message("mr_o2o(\"snp_file_1\", \"snp_file_2\")")
-  message("")
-  message("可以传入的参数:")
+  message("两样本")
+  message("mr_common(\"ieu-a-2\", \"ieu-a-2\")")
   message("p1 = 5e-8, p2=5e-5,\nwrite_csv=TRUE, write_ppt=TRUE,\nmethod_list = c(\"mr_ivw\",\"mr_egger_regression\",\"mr_weighted_median\", \"mr_weighted_mode\"),\nrm_snps=NULL")
   message("")
-  message("csv 文件的表头格式一定是:")
+  message("中介孟德尔")
+  message("mr_intermediary(\"ebi-a-GCST001475\", \"ukb-a-328\", \"ieu-b-4954\")")
+  message("多变量孟德尔")
+  message("mr_multi(c(\"ieu-a-2\", \"ieu-b-2\"), \"ebi-a-GCST011081\")")
+  message("药靶孟德尔")
+  message("mr_common(\"ieu-a-300\", \"ieu-a-7\", r2=0.3, kb=100, gene=\"HMGCR\", eaf_threshold = 0.01)")
+
+  message("本地数据文件的表头格式一定是:")
   message("SNP,beta,se,effect allele,other allele,pval,eaf")
+  message("根据原数据的变量含义修改")
 }
