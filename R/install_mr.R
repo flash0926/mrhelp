47e6b37088fcf08e8ee20a212e909d8275851e4c
flash0926 <675245820@qq.com>
2023-08-24 09:09:50 +0800

    Update install_mr.R
    121

--- R/install_mr.R
+++ R/install_mr.R
@@ -5,7 +5,7 @@
 #'
 #' @examples install_mr()
 install_mr <- function(){
-  packages <- c("usethis", "RPostgreSQL","devtools", "remotes", "data.table", "do", "eoffice", "epigraphdb")
+  packages <- c("usethis", "devtools", "remotes","data.table", "do", "eoffice", "epigraphdb", "metafor", "psych","ggplot2","gdata","RMySQL")
 
   for (i in 1:length(packages)) {
     if (!packages[i] %in% installed.packages()[,"Package"]) {
